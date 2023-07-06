3f193a26aca562e4d5bc1d3caaed05b108773cc0
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-07-06 16:30:10 +0200

    Start version

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,12 +6,9 @@ use App\Entity\Article;
 use App\Enum\ArticleStatusEnum;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/articles', name: 'articles_')]
 class ArticleController extends AbstractController
 {
-    #[Route('/list/{status}', name: 'list', methods: ['GET', 'POST'])]
     public function list(ArticleStatusEnum $status = ArticleStatusEnum::PUBLISHED): Response
     {
         return $this->render('article/index.html.twig', [
@@ -20,7 +17,6 @@ class ArticleController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'], methods: ['GET'], condition: 'params["id"] >= 1', priority: 1)]
     public function show(?Article $article): Response
     {
         return $this->render('article/show.html.twig', [
@@ -29,7 +25,6 @@ class ArticleController extends AbstractController
         ]);
     }
 
-    #[Route('/{slug}', name: 'show_slug', methods: ['GET'])]
     public function showWithSlug(?Article $article): Response
     {
         return $this->render('article/show.html.twig', [
@@ -38,12 +33,6 @@ class ArticleController extends AbstractController
         ]);
     }
 
-    #[Route(
-        '/{id<\d+>}',
-        name: 'delete',
-        methods: ['DELETE'],
-        condition: 'request.headers.get("X-Requested-With") == "XMLHttpRequest" and params["id"] >= 1',
-    )]
     public function delete(): Response
     {
         return $this->json([
